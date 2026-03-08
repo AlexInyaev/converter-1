2ed4f97779d0eaa193ce79266aeb22c896447e2e
Alex <alinyaev494@gmail.com>
2026-03-08 23:54:03 +0300

    Второе домашнее задание

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 const (
 	fromUSDtoEUR = 0.86
 	fromUSDtoRUB = 81.13
 )
+
+func getDataFromUser() (int, int, int) {
+	var (
+		amount       int
+		currencyFrom int
+		currencyTo   int
+	)
+	_, err := fmt.Scan(&amount)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = fmt.Scan(&currencyFrom)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = fmt.Scan(&currencyTo)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return amount, currencyFrom, currencyTo
+}
+func counter(amount int, currencyFrom int, currencyTo int) int {
+	return 0
+}
 
 func main() {
 	fromEURtoRUB := fromUSDtoRUB / fromUSDtoEUR
